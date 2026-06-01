1823edcfbaa2aabf253a24bf8d6fdc1198ccd287
Gavin Collins <gavincollins11.11@gmail.com>
2026-06-01 14:47:21 +0800

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :documents do
+    resources :clauses, only: [:index, :show]
+    resources :chats, only: [:show, :create] do
+      resources :messages, only: [:create]
+    end
+  end
+
+  resources :doc_files, only: [:create, :destroy]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
--- db/migrate/20260601050233_devise_create_users.rb
+++ db/migrate/20260601050233_devise_create_users.rb
@@ -32,6 +32,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[8.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      ## custom columns
+      t.string :user_name
 
       t.timestamps null: false
     end
