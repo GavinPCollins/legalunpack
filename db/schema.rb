714b30219f4231415553cce71b7fd4b2f5efbc1d
Gavin Collins <gavincollins11.11@gmail.com>
2026-06-01 19:07:34 +0800

    basic routes added

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_01_070000) do
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
     t.datetime "updated_at", null: false
+    t.string "user_name"
     t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
