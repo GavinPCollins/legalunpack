b24b68e75569668fda788d98f4b71d91c1589352
George Baum <george@georgebaum.me.uk>
2026-06-01 16:13:17 +1000

    complete relation

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,5 @@
 class Document < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, foreign_key: :id_user
 
   has_many :doc_files, dependent: :destroy
   has_many :clauses, dependent: :destroy
