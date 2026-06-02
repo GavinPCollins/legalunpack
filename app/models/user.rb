21172832c8553831a91d7aae2834320dae2e9008
Halo Arieli <hila.m.arieli@gmail.com>
2026-06-02 14:53:04 +1000

    login work, with tailwind UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,27 @@
 class User < ApplicationRecord
+  attr_writer :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+
   has_many :packages, dependent: :destroy
   has_many :chats, through: :packages
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)&.downcase
+
+    where(conditions).where(
+      "lower(email) = :value OR lower(username) = :value",
+      value: login
+    ).first
+  end
 end
