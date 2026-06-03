4263db6bc225deb64bf585526882533c35b54088
Gavin Collins <gavincollins11.11@gmail.com>
2026-06-04 07:07:05 +0800

    Update package document management UI

---
+++ test/controllers/doc_files_controller_test.rb
@@ -0,0 +1,45 @@
+# CODEX add document
+require "test_helper"
+
+class DocFilesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = User.create!(email: "doc-files@example.com", password: "password", username: "docfiles")
+    @package = @user.packages.create!(name: "Lease review")
+
+    sign_in @user
+  end
+
+  test "should add uploaded document to current user package" do
+    uploaded_file = fixture_file_upload("sample.txt", "text/plain")
+
+    assert_difference("DocFile.count", 1) do
+      post doc_files_url, params: { package_id: @package.id, files: [uploaded_file] }
+    end
+
+    assert_redirected_to package_url(@package)
+    assert_equal "sample.txt", @package.doc_files.last.file.filename.to_s
+  end
+
+  test "should reject add document without a file" do
+    assert_no_difference("DocFile.count") do
+      post doc_files_url, params: { package_id: @package.id }
+    end
+
+    assert_response :unprocessable_entity
+    assert_includes response.body, "Choose at least 1 file"
+  end
+
+  test "should not add document to another user's package" do
+    other_user = User.create!(email: "other-doc-files@example.com", password: "password", username: "otherdocfiles")
+    other_package = other_user.packages.create!(name: "Private package")
+    uploaded_file = fixture_file_upload("sample.txt", "text/plain")
+
+    assert_no_difference("DocFile.count") do
+      post doc_files_url, params: { package_id: other_package.id, files: [uploaded_file] }
+    end
+
+    assert_response :not_found
+  end
+end
