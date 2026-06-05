88da18fbe75257f8e8dca09855256ee19d11d2b6
Gavin Collins <gavincollins11.11@gmail.com>
2026-06-05 11:02:28 +0800

    summary_page

--- app/controllers/doc_files_controller.rb
+++ app/controllers/doc_files_controller.rb
@@ -14,6 +14,16 @@ class DocFilesController < ApplicationController
     end
   end
 
+  # summary page update
+  def summary
+    @doc_file = DocFile
+                .joins(:package)
+                .where(packages: { user_id: current_user.id })
+                .includes(:clauses, file_attachment: :blob, package: {})
+                .find(params[:id])
+    @package = @doc_file.package
+  end
+
   def destroy
     doc_file = DocFile.joins(:package).where(packages: { user_id: current_user.id }).find(params[:id])
     package = doc_file.package
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,9 @@ Rails.application.routes.draw do
   # end
 
   # CODEX add document updates
-  resources :doc_files, only: [:create, :destroy]
+  resources :doc_files, only: [:create, :destroy] do
+    get :summary, on: :member
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
